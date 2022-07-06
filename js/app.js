7717585b11ae83f2c958babfb5e200b010593ed1
GreenDevald1523 <46032258+GreenDevald1523@users.noreply.github.com>
2022-07-06 18:24:43 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,36 @@
+for (let i = 1; i <= 52; i++) {
+  $(`#btn${i}`).click(function () {
+    if ($(`#openBody${i}`).hasClass("mb-30")) {
+      $(`#openBody${i}`).removeClass("mb-30");
+    } else {
+      $(`#openBody${i}`).addClass("mb-30");
+    }
+    if ($(`#btn${i}`).hasClass("hidden")) {
+      $(`#btn${i}`).removeClass("hidden");
+      $(`#btnClose${i}`).addClass("hidden");
+    } else {
+      $(`#btn${i}`).addClass("hidden");
+      $(`#btnClose${i}`).removeClass("hidden");
+    }
+    $(`#btn${i}`).addClass("pos-r")
+    $(`#open${i}`).slideToggle(500);
+  });
+  $(`#btnClose${i}`).click(function () {
+    if ($(`#openBody${i}`).hasClass("mb-30")) {
+      $(`#openBody${i}`).removeClass("mb-30");
+    } else {
+      $(`#openBody${i}`).addClass("mb-30");
+    }
+    if ($(`#btn${i}`).hasClass("hidden")) {
+      $(`#btn${i}`).removeClass("hidden");
+      $(`#btnClose${i}`).addClass("hidden");
+    } else {
+      $(`#btn${i}`).addClass("hidden");
+      $(`#btnClose${i}`).removeClass("hidden");
+    }
+    $(`#open${i}`).slideToggle(500);
+    setTimeout(() => {
+        $(`#btn${i}`).removeClass("pos-r")
+   }, 485)
+  });
+}
